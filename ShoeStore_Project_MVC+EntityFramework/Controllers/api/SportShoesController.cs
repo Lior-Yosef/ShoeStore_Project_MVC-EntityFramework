1d8bf3041f738bdaa5c33925a221492f4b2e250c
Lior-Yosef <lioryosef06@gmail.com>
2022-01-17 14:24:38 +0200

    add put action

--- ShoeStore_Project_MVC+EntityFramework/Controllers/api/SportShoesController.cs
+++ ShoeStore_Project_MVC+EntityFramework/Controllers/api/SportShoesController.cs
@@ -54,22 +54,81 @@ namespace ShoeStore_Project_MVC_EntityFramework.Controllers.api
         {
             try
             {
+                using(SqlConnection conn = new SqlConnection(connctionString))
+                {
+                    conn.Open();
 
-                return Ok();
+                    string query = $@"SELECT * FROM SportShoes WHERE Id={id}";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader DataFromDB = cmd.ExecuteReader();
+                    if (DataFromDB.HasRows)
+                    {
+                        while (DataFromDB.Read())
+                        {
+                            SportShoe objectSportShoe = new SportShoe(DataFromDB.GetInt32(0), DataFromDB.GetString(1), DataFromDB.GetString(2), DataFromDB.GetInt32(3), DataFromDB.GetInt32(4));
+                            conn.Close();
+                            return Ok(objectSportShoe);
+                        }
+                    }
+                    else
+                    {
+                        string respons = "not found";
+                        conn.Close();
+                        return Ok(new { respons });
+                    }
+                }
             }
             catch (SqlException ex) { return BadRequest(ex.Message); }
 
             catch (Exception ex) { return BadRequest(ex.Message); }
+            return Ok();
         }
 
         // POST: api/SportShoes
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody] SportShoe value)
         {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connctionString))
+                {
+                    conn.Open();
+                    string query = $@"INSERT INTO SportShoes
+                                    VALUES( '{value.CompanyName}','{value.ModelName}','{value.Size}','{value.Price}')";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    int row = cmd.ExecuteNonQuery();
+                    return Get();
+                }
+            }
+            catch (SqlException ex) { return BadRequest(ex.Message); }
+
+            catch (Exception ex) { return BadRequest(ex.Message); }
+
         }
 
         // PUT: api/SportShoes/5
-        public void Put(int id, [FromBody]string value)
+        public IHttpActionResult Put(int id, [FromBody] SportShoe value)
         {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connctionString))
+                {
+                    conn.Open();
+                    string Putquery = $@"UPDATE SportShoes
+                                        SET CompanyName='{value.CompanyName}',ModelName='{value.ModelName}',Size='{value.Size}',Price='{value.Price}'
+                                         WHERE Id={id}";
+                    SqlCommand cmd = new SqlCommand(Putquery, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    conn.Close();
+                    return Ok(Get());
+
+                }
+
+            }
+            catch (SqlException ex) { return BadRequest(ex.Message); }
+
+            catch (Exception ex) { return BadRequest(ex.Message); }
+
+
         }
 
         // DELETE: api/SportShoes/5
